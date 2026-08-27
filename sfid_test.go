ee3518ce931837ad6169c3fa80ce07abe0c4a44c
Alexander Crisp <alex@crisp.codes>
2026-08-27 13:45:36 +0100

    Add parse tests

--- sfid_test.go
+++ sfid_test.go
@@ -15,41 +15,60 @@ var (
 	_ json.Unmarshaler = (*sfid.ID)(nil)
 )
 
-func TestID_UnmarshalJSON(t *testing.T) {
-	tests := []struct {
-		name     string
-		input    string
-		isValid  bool
-		expected sfid.ID
-	}{
-		{
-			name:     "18 char valid",
-			input:    `"012000000000000AAA"`,
-			isValid:  true,
-			expected: sfid.ID("012000000000000AAA"),
-		},
-		{
-			name:    "18 char invalid",
-			input:   `"012000000000000ZZZ"`,
-			isValid: false,
-		},
-		{
-			name:     "15 char valid",
-			input:    `"012000000000000"`,
-			isValid:  true,
-			expected: sfid.ID("012000000000000AAA"),
-		},
-		{
-			name:    "empty",
-			input:   `""`,
-			isValid: false,
-		},
-	}
+var parseTests = []struct {
+	name     string
+	input    string
+	isValid  bool
+	expected sfid.ID
+}{
+	{
+		name:     "18 char valid",
+		input:    `012000000000000AAA`,
+		isValid:  true,
+		expected: sfid.ID("012000000000000AAA"),
+	},
+	{
+		name:    "18 char invalid",
+		input:   `012000000000000ZZZ`,
+		isValid: false,
+	},
+	{
+		name:     "15 char valid",
+		input:    `012000000000000`,
+		isValid:  true,
+		expected: sfid.ID("012000000000000AAA"),
+	},
+	{
+		name:    "empty",
+		input:   ``,
+		isValid: false,
+	},
+}
 
-	for _, test := range tests {
+func Test_Parse(t *testing.T) {
+	for _, test := range parseTests {
+		t.Run(test.name, func(t *testing.T) {
+			id, err := sfid.Parse(test.input)
+			if test.isValid {
+				if err != nil {
+					t.Errorf("unexpected error: %v", err)
+				} else if id != test.expected {
+					t.Errorf("unexpected ID: %v", id)
+				}
+			} else {
+				if !errors.Is(err, sfid.ErrInvalidID) {
+					t.Errorf("expected invalid ID error, got: %v", err)
+				}
+			}
+		})
+	}
+}
+
+func TestID_UnmarshalJSON(t *testing.T) {
+	for _, test := range parseTests {
 		t.Run(test.name, func(t *testing.T) {
 			var id sfid.ID
-			err := json.Unmarshal([]byte(test.input), &id)
+			err := json.Unmarshal([]byte(fmt.Sprintf(`"%s"`, test.input)), &id)
 			if test.isValid {
 				if err != nil {
 					t.Errorf("unexpected error: %v", err)
